b2456b967938f723c43732939508ce9d8c68fe72
Rookie <gb880327@189.cn>
2017-09-28 16:36:59 +0800

    新增系统日志管理界面

---
+++ com.warrior.web/src/main/webapp/WEB-INF/views/src/views/log/expand-row.vue
@@ -0,0 +1,34 @@
+<style scoped>
+    .expand-row{
+        margin-bottom: 16px;
+    }
+</style>
+<template>
+    <div>
+        <Row class="expand-row">
+            <Col span="12">
+                <span class="expand-key">操作用户ID：</span>
+                <span class="expand-value">{{ row.userId }}</span>
+            </Col>
+        </Row>
+        <Row class="expand-row">
+          <Col span="24">
+              <span class="expand-key">执行方法：</span>
+              <span class="expand-value">{{ row.method }}</span>
+          </Col>
+        </Row>
+        <Row class="expand-row">
+          <Col span="24">
+              <span class="expand-key">请求参数：</span>
+              <span class="expand-value">{{ row.params }}</span>
+          </Col>
+        </Row>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            row: Object
+        }
+    };
+</script>
